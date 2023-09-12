2cfd90009b982547cf4c65a57dd9c9c859d8a724
moara <moaramirandass@gmail.com>
2023-09-12 15:29:06 +0200

    managing images upload

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
-
+const uploadConfig = require("./configs/upload");
 const AppError = require("./utils/AppError");
 const express = require("express");
 
+const cors = require("cors");
 const routes = require("./routes");
-migrationsRun();
-const app = express();
 
+const app = express();
+migrationsRun();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 // express-async-errors handle errors
 app.use((error, request, response, next) => {
-  console.error(error)
+  console.error(error);
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
